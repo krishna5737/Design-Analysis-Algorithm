41b3f88b7692d77bdbc09abc9907d92b3c3c5667
krishna5737 <iro2015007@iiita.ac.in>
2017-09-02 20:20:24 +0530

    Create p1.py

---
+++ Course3/week1/p1.py
@@ -0,0 +1,16 @@
+with open("jobs.txt", 'r') as fileObj:
+        lines = fileObj.readlines()
+        numJobs = int(lines[0].strip())
+        jobList = [(int(line.split()[0]),int(line.split()[1])) for line in lines[1:] ] 
+        
+JobList = [ (w, l, (w-l)) for w, l in jobList ]
+JobList.sort(key=lambda x: x[0], reverse=True)   
+JobList.sort(key=lambda x: x[2], reverse=True)   
+
+ls = 0
+ws = 0
+for job in JobList:
+    ls += job[1]
+    ws += job[0] * ls
+
+print(ws)
